c6b3356c62af1963786076217a48c33d735f98f5
Surjith Senthilkumar <senthilkumar.sur@northeastern.edu>
2024-07-25 21:40:59 +0530

    updated magic-button

--- components/MagicButton.tsx
+++ components/MagicButton.tsx
@@ -21,22 +21,32 @@ const MagicButton = ({
   otherClasses?: string;
 }) => {
   return (
-    <button
-      className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
-      onClick={handleClick}
-    >
-      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+    // <button
+    //   className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
+    //   onClick={handleClick}
+    // >
+    //   <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
 
-      {/* remove px-3 py-1, add px-5 gap-2 */}
-      <span
-        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
-             bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
-      >
-        {position === "left" && icon}
-        {title}
-        {position === "right" && icon}
-      </span>
-    </button>
+    //   {/* remove px-3 py-1, add px-5 gap-2 */}
+    //   <span
+    //     className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
+    //          bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
+    //   >
+    //     {position === "left" && icon}
+    //     {title}
+    //     {position === "right" && icon}
+    //   </span>
+    // </button>
+
+        // Button code
+        <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          {position === "left" && icon}
+          {title}
+          {position === "right" && icon}
+        </button>
+  
+       
+      
   );
 };
 
